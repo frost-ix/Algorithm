2518ed7af17ccb6ac6145a070c30197f9a3e37a8
frost-secu <85593989+frost-secu@users.noreply.github.com>
2022-11-13 22:56:06 +0900

    ch_h

--- Sort/header.h
+++ Sort/header.h
@@ -1,8 +1,18 @@
 #include <iostream>
 #include <cstdlib>
+#include <algorithm>
+#include <random>
+
+// Redesign change module
+#define CHANGE(index_i, index_j, temp) ((temp)=(index_i), (index_i)=(index_j), (index_j)=(temp))
 
 void sort_board();
+void sort_intro();
 
-void bubble();
-void selection();
-void insertion();
+void data_input(int* arr, int size);
+
+void getArrData(int* arr, int size, int cycle);
+
+int bubble(int* arr, int size, int cycle);
+int selection(int* arr, int size, int cycle);
+int insertion(int* arr, int size,int cycle);
